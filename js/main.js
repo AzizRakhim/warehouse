56e4311d4f6c91b755f75f58f8acf7c3fe032e32
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-02-22 01:23:20 +0500

    finished header

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let elSettingBtn = document.getElementById("settingBtn");
+let elSettingMenu = document.getElementById("settingMenu");
+
+elSettingBtn.addEventListener("click", function() {
+  elSettingMenu.classList.toggle("appear");
+})
